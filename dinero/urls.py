2765fd033159466b49b9f79418a40491ea07ff22
David Cho <dc664@cornell.edu>
2013-08-02 17:05:51 -0400

    need to sort array

--- dinero/urls.py
+++ dinero/urls.py
@@ -8,15 +8,17 @@ from dinero import settings
 
 urlpatterns = patterns('',
 	url(r'^$', home),
-    # Examples:
-    # url(r'^$', 'dinero.views.home', name='home'),
-    # url(r'^dinero/', include('dinero.foo.urls')),
+    url(r'^entries/$', entries),
+
+    # url(r'^ajaxexample$', main),
+    # url(r'^ajaxexample_json$', ajax),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+
     # static files for all pages
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', 
         {'document_root': settings.STATIC_ROOT}),
--- dinero/views.py
+++ dinero/views.py
@@ -1,36 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from entry.models import Entry
 import re
+import django.utils.simplejson as json
 
 def home(request):
 	# parse()
 	errors = {}
 	if 'submit' in request.POST:
 		# Doing server side validation of the form
-		country = request.POST['country']
-		if not country or not re.match("^\D+$", country):
-			errors['country'] = True
-		else:
-			country = country.title()
-
-		if request.POST['denomination']:
-			try:
-				denom = int(request.POST['denomination'])
-			except ValueError:
-				errors['denom'] = True
-		else:
-			errors['denom'] = True
-
-		if request.POST['quantity']:
-			try:
-				quantity = int(request.POST['quantity'])
-			except ValueError:
-				errors['quantity'] = True
-		else:
-			errors['quantity'] = True
-
+		errors = validate(request)
 		if not errors:
 			try:
+				country = request.POST['country'].title()
+				denom = int(request.POST['denomination'])
+				quantity = int(request.POST['quantity'])
+
 				entry = Entry.objects.get(country=country, denomination=denom)
 				entry.quantity += quantity
 				entry.save()
@@ -42,11 +27,19 @@ def home(request):
 		print 'saving changes now'
 		if request.POST.get('text', ''):
 			data = request.POST['text']
-			print data
 		else:
 			print 'nothing'
 
 	entries = Entry.objects.all().order_by('country', 'denomination')
+	response = []
+	for entry in entries:
+		obj = {}
+		obj['currency'] = entry.country
+		obj['denomination'] = entry.denomination
+		obj['quantity'] = entry.quantity
+		response.append(obj)
+	string = json.dumps(response)
+
 	return render(request, 'home.html', {
 		'entries' : entries,
 		'rawtext' : read(),
@@ -77,3 +70,56 @@ def read():
 	string = f.read()
 	f.close()
 	return string
+
+def validate(request):
+	errors = {}
+	country = request.POST['country']
+	if not country or not re.match("^\D+$", country):
+		errors['country'] = True
+
+	if request.POST['denomination']:
+		try:
+			denom = int(request.POST['denomination'])
+		except ValueError:
+			errors['denom'] = True
+	else:
+		errors['denom'] = True
+
+	if request.POST['quantity']:
+		try:
+			quantity = int(request.POST['quantity'])
+		except ValueError:
+			errors['quantity'] = True
+	else:
+		errors['quantity'] = True
+
+	return errors
+
+def entries(request):
+	response = []
+	entries = Entry.objects.all()#.order_by('country', 'denomination')
+	for entry in entries:
+		obj = {}
+		obj['currency'] = entry.country
+		obj['denomination'] = entry.denomination
+		obj['quantity'] = entry.quantity
+		response.append(obj)
+	return HttpResponse(json.dumps(response), content_type="application/json")
+
+# from django.shortcuts import render_to_response
+# from django.template import RequestContext
+# from django.utils import simplejson
+# import socket
+ 
+# def main(request):
+# 	return render_to_response('ajaxexample.html', context_instance=RequestContext(request))
+ 
+# def ajax(request):
+# 	if request.POST.has_key('client_response'):
+# 		x = request.POST['client_response']                 
+# 		y = "hello %s!" % x                         
+# 		response_dict = {}                                         
+# 		response_dict.update({'server_response': y })                                                                  
+# 		return HttpResponse(simplejson.dumps(response_dict), mimetype='application/javascript')
+# 	else:
+# 		return render_to_response('ajaxexample.html', context_instance=RequestContext(request))
